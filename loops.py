cda507bf1d89719f092572f2dd33aeddcab90fea
Harsha <harshaa.bsc23@rvu.edu.in>
2026-02-18 22:17:37 +0530

    day 5

---
+++ loops.py
@@ -0,0 +1,18 @@
+#for loop
+mylist=["apple","banana","cherry"]
+for x in mylist:
+    print (x)
+
+#for loop through indexing
+
+mylist_1=["apple","banana","kiwi","cherry"]
+for i in range(len(mylist_1)):
+    print(mylist_1[i])
+
+#while loop
+
+mylist_2=["apple","banana","kiwi","strawberry"]
+i=0
+while i<len(mylist_2):
+    print(mylist_2[i])
+    i=i+1
